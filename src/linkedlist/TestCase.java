76640598635333561739aab1ad3e47213298ff32
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-20 20:38:12 +0530

    add

--- src/linkedlist/TestCase.java
+++ src/linkedlist/TestCase.java
@@ -1,62 +1,47 @@
 package linkedlist;
 
 import static org.junit.Assert.*;
-
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-
 import main.LinkedListClass;
 import main.LinkedListNode;
-
 public class GenericLinkedListTest {
- private LinkedListNode<Integer> myFirstNode ;
- private LinkedListNode<Integer> mySecondNode ;
- private LinkedListNode<Integer> myThirdNode ;
-
 	LinkedListClass<Integer> LinkedList;
-	private LinkedListNode<Integer> myFirstNode;
-	private LinkedListNode<Integer> mySecondNode;
-	private LinkedListNode<Integer> myThirdNode;
-
+	static private LinkedListNode<Integer> myFirstNode;
+	static private LinkedListNode<Integer> mySecondNode;
+	static private LinkedListNode<Integer> myThirdNode;
 	@Before
 	public void init() {
-	myFirstNode = new LinkedListNode<Integer>(70);
+	myFirstNode = new LinkedListNode<Integer>(56);
 	mySecondNode = new LinkedListNode<Integer>(30);
 	myThirdNode = new LinkedListNode<Integer>(70);
 	LinkedList= new LinkedListClass<Integer>();
 
-	LinkedList.append(myThirdNode);
+	LinkedList.insert(myFirstNode);
 	LinkedList.append(mySecondNode);
-	LinkedList.append(myFirstNode);
+	LinkedList.append(myThirdNode);
+	LinkedList.push(myFirstNode,mySecondNode);
+	LinkedList.printlist();
+	LinkedList.pop();
 	}
 
 	@Test
 	public void ReturnTrueWhenMyFirstNodeAppend() {
 		Assert.assertTrue((LinkedList.head).equals(myFirstNode));}
+	public void mySecondNodebecomesHeadwhenmyFirstNodeisPopped() {
+		Assert.assertTrue((LinkedList.head).equals(mySecondNode));}
 	@Test
-	public void test() {
-
-	//Inserting the values
-		myFirstNode = new LinkedListNode<Integer>(56);
-		mySecondNode=new LinkedListNode<Integer>(30);
-		myThirdNode=new LinkedListNode<Integer>(70);
-	//Linking the nodes
-		myFirstNode.setNext(mySecondNode);
-		mySecondNode.setNext(myThirdNode);
 	public void ReturnTrueWhenMySecondNodeAppend() {
 		Assert.assertTrue(((LinkedList.head).getNext()).equals(mySecondNode));}
 	@Test
 		public void ReturnTrueWhenMyThirdNodeAppend() {
 		Assert.assertTrue((LinkedList.tail).equals(myThirdNode));
 		}	
+	public void myThirdNodebecomesmySecondNodewhenmyFirstNodeisPopped() {
+		Assert.assertTrue(((LinkedList.head).getNext()).equals(myThirdNode));}		}	
 
 
-	boolean result= myFirstNode.getNext().equals(mySecondNode)&&
-					mySecondNode.getNext().equals(myThirdNode);
-	Assert.assertTrue(result);
 
 
-	}
-}
+
