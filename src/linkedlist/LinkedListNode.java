5d221bc918430a241b514eb6308b8dc20ac4d6de
RUCHIT680 <ruchitrai282@gmail.com>
2020-10-20 20:49:49 +0530

    add

--- src/linkedlist/LinkedListNode.java
+++ src/linkedlist/LinkedListNode.java
@@ -11,9 +11,12 @@ public class LinkedListClass<T> {
 		// TODO Auto-generated constructor stub
 	}
 	
+
 	public void insert(ILinkedList<T> newNode) {
 		if (this.tail==null) {
 			this.tail=newNode;
+		if (this.tail == null) {
+			this.tail = newNode;
 		}
 		
 		if(this.head==null) {
@@ -21,16 +24,7 @@ public class LinkedListClass<T> {
 		}else {
 			ILinkedList<T> temp=this.head;
 			this.head=newNode;
-			(this.head).setNext(temp);
-		}
-	}
-	
-	
-		
-	public void append(ILinkedList<T> newNode) {
-		if (this.tail == null) {
-			this.tail = newNode;
-		}
+
 		if (this.head == null) {
 			this.head = newNode;
 		} else {
@@ -38,55 +32,97 @@ public class LinkedListClass<T> {
 			this.head = newNode;
 			(this.head).setNext(temp);
 		}
-	}
+	
+
+
+
+
+	public void append(ILinkedList<T> newNode) {
+		if (this.tail == null) {
+			this.tail = newNode;
+@@ -44,61 +42,69 @@ public void append(ILinkedList<T> newNode) {
+
 	public void printlist() {
 		System.out.print("Linked List=");
 		ILinkedList<T> temp=head;
+		ILinkedList<T> temp = head;
 		do {
 			System.out.println(temp.getT());
 			if(!temp.equals(tail)) System.out.println(">");
 			temp=temp.getNext();
 		}while(temp.getNext()!= null);
+			if (!temp.equals(tail))
+				System.out.println(">");
+			temp = temp.getNext();
+		} while (temp.getNext() != null);
 		System.out.println(temp.getT());
+	}
+
+	public void push(ILinkedList<T> beforeNode, ILinkedList<T> newNode) {
+		ILinkedList<T> temp = beforeNode.getNext();
+		beforeNode.setNext(newNode);
+		newNode.setNext(temp);
+	}
+
+	public void pop() {
+		ILinkedList<T> temp = head;
+		head = head.getNext();
+
+	}
+
+	public void poptail() {
+		ILinkedList<T> temp = head;
+		while ((!(temp.getNext()).equals(tail))) {
+			temp = temp.getNext();
 		}
-	
+
  public void push(ILinkedList<T> beforeNode,ILinkedList<T> newNode) {
 	 ILinkedList<T> temp=beforeNode.getNext();
 	 beforeNode.setNext(newNode);
 	 newNode.setNext(temp);
  }
- 
+
  public void pop() {
 	 ILinkedList<T> temp=head;
 	 head=head.getNext();
-	 
+
  }
-	
+
 public void poptail() {
 	ILinkedList<T> temp=head;
 	while((!(temp.getNext()).equals(tail))) {
 		temp=temp.getNext();
+		tail = temp;
+		tail.setNext(null);
+
 	}
 	tail=temp;
 	tail.setNext(null);
 
 }
 
-public int search(T data) {
 public LinkedListNode<T> search(T data) {
 	ILinkedList<T> temp=head;
 	int flag=1;
 	while(temp!=null && temp.getNext()!=null) {
 		if((temp.getT()).equals(data)) {
-			return flag;
 			return (LinkedListNode<T>) temp;
+	public LinkedListNode<T> search(T data) {
+		ILinkedList<T> temp = head;
+		int flag = 1;
+		while (temp != null && temp.getNext() != null) {
+			if ((temp.getT()).equals(data)) {
+				return (LinkedListNode<T>) temp;
+			}
+
+			temp = temp.getNext();
+
 		}
-		flag ++;
 
 		temp=temp.getNext();
 
+		return (LinkedListNode<T>) tail;
 	}
-	return -1;
 	return (LinkedListNode<T>) tail;
 }
 public void insertAfter(T before_data, T data) {
@@ -97,5 +133,27 @@ public void insertAfter(T before_data, T data) {
 
 
 }
+
+}
+
+
+	public void insertAfter(T before_data, T data) {
+		LinkedListNode<T> before_Node = search(before_data);
+		LinkedListNode<T> newNode = new LinkedListNode<T>(data);
+
+		push(before_Node, newNode);
+	}
+
+	public int deleteNode(T value) {
+		ILinkedList<T> temp = head;
+		int count = 1;
+		while (temp != null && temp.getNext() != null) {
+			temp.setNext((temp.getNext()).getNext());
+		}
+		count++;
+		temp = temp.getNext();
+
+		return count;
+	}
 
 }
